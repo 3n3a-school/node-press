129b96f3d5de583c3a9f14800fe390285609f3c9
3n3a <gh@3n3a.ch>
2022-06-25 13:29:56 +0000

    add auth service, component and guard

---
+++ frontend/src/app/pages/login/login.component.ts
@@ -0,0 +1,26 @@
+import { Component, OnInit } from '@angular/core';
+
+import { FormGroup, FormControl } from '@angular/forms';
+
+@Component({
+  selector: 'app-login',
+  templateUrl: './login.component.html',
+  styleUrls: ['./login.component.scss']
+})
+export class LoginComponent implements OnInit {
+
+  loginForm = new FormGroup({
+    username: new FormControl(''),
+    password: new FormControl(''),
+  })
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+  onSubmit() {
+    window.alert(`u: ${this.loginForm.value.username} p: ${this.loginForm.value.password}`)
+  }
+
+}
---
+++ frontend/src/app/services/auth.service.ts
@@ -0,0 +1,45 @@
+import { Injectable } from '@angular/core';
+import { User } from '../models/user';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AuthService {
+
+  STORE_KEY = "nodepress-auth"
+
+  constructor() { }
+
+  isLoggedIn() {
+    const authKey = localStorage.getItem(this.STORE_KEY)
+    return authKey != null && authKey != ""
+  }
+
+  login(user: User) {
+    // TODO: implement login function
+    const success = true
+    const token = "ldjfdskfjdlfj"
+    if (success) {
+      localStorage.setItem(this.STORE_KEY, token)
+    } else {
+      localStorage.setItem(this.STORE_KEY, "")
+    }
+    return success
+  }
+
+  register(user: User) {
+    // TODO: implement register function
+    const success = true
+    const token = "ldfjdlfk"
+    if (success) {
+      localStorage.setItem(this.STORE_KEY, token)
+    } else {
+      localStorage.setItem(this.STORE_KEY, "")
+    }
+    return success
+  }
+
+  logout() {
+    localStorage.removeItem(this.STORE_KEY)
+  }
+}
